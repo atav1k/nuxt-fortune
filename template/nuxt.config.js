23bb57293b02bd6514240171fa24e7d010671ead
Vikram Babu <atav1k@users.noreply.github.com>
2018-04-16 13:49:33 -0700

    add markdownit & fortune

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
+  /* Site css */
+  css: [
+    { src: '~/assets/app.css' }
+  ],
+
+  /* Headers of the page */
   head: {
     title: '{{ name }}',
     meta: [
@@ -10,20 +13,27 @@ module.exports = {
       { hid: 'description', name: 'description', content: '{{escape description }}' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/fortune-css/dist/fortune.min.css' }
     ]
   },
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
+  /* Customize the progress bar color */
+  loading: { color: 'var(--state-primary)' },
+
+  /* Build configuration*/
   build: {
-    /*
-    ** Run ESLint on save
-    */
+    postcss: {
+      plugins: {
+      'postcss-cssnext': {
+        features: {
+          rem: false,
+          customProperties: false
+          }
+        }
+      }
+    },
+
+    /* Run ESLint on save */
     extend (config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
